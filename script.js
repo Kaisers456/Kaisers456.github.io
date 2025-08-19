d8cb70c34d01e639c0061a04cc2ef6a36f3ef48a
Kaisers456 <shibghatullahkaiser@gmail.com>
2025-08-19 09:45:50 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -587,10 +587,10 @@ scene.add(planet);
 
 //Ganti Deskripsi Planet
 const ringTexts = [
-  'Galaxy of love From ...',//untuk deskripsi planet layer 1
+  'For my Motki Sona Kayla',//untuk deskripsi planet layer 1
   "I love you",//untuk deskripsi planet layer 2
-  "♡Happy Girlfriend day♡",//untuk deskripsi planet layer 3
-  "01/08/2025",//untuk deskripsi planet layer 4
+  "I love you so much♡",//untuk deskripsi planet layer 3
+  "aku sayang kamuuu",//untuk deskripsi planet layer 4
   ...(window.dataCCD && window.dataCCD.data.ringTexts ? window.dataCCD.data.ringTexts : [])
 ];
 
